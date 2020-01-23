970e2a2d2039923cc4cd26243e2cee66e0215330
Sravani Alwala <sravani.alwala@SLS.ads.valuelabs.net>
2020-01-23 12:45:49 +0530

    Updates

---
+++ precache-manifest.5a909d6875f66d4335ac1e468aba546a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a058b0f53b73a7469bc28c2647f3e5",
+    "url": "/ghpages_omdb/index.html"
+  },
+  {
+    "revision": "f5782df587bb741e8512",
+    "url": "/ghpages_omdb/static/css/main.750bf7e9.chunk.css"
+  },
+  {
+    "revision": "8c58a5ce9963e8501897",
+    "url": "/ghpages_omdb/static/js/2.36d3fbde.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ghpages_omdb/static/js/2.36d3fbde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5782df587bb741e8512",
+    "url": "/ghpages_omdb/static/js/main.23f70050.chunk.js"
+  },
+  {
+    "revision": "8e7d79ba1fdf173f6f5d",
+    "url": "/ghpages_omdb/static/js/runtime-main.db47492d.js"
+  },
+  {
+    "revision": "f233920825854a0d755e4b1e83ae7cdd",
+    "url": "/ghpages_omdb/static/media/army.f2339208.png"
+  },
+  {
+    "revision": "6d33bc3a9744759e6cdae86d9661bf8b",
+    "url": "/ghpages_omdb/static/media/cross.6d33bc3a.png"
+  },
+  {
+    "revision": "6e673bc0954201d2dbccb9c8d98d5186",
+    "url": "/ghpages_omdb/static/media/imdb1.6e673bc0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages_omdb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6525eb42e89e342fa1a0f0b8908bffbe",
+    "url": "/ghpages_omdb/static/media/noimg.6525eb42.png"
+  },
+  {
+    "revision": "bffa89cf53cb96465084698fc20bc508",
+    "url": "/ghpages_omdb/static/media/search.bffa89cf.png"
+  }
+]);
